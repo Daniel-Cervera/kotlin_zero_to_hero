7b0e9c34d0b55e9847cfb563ed2cfb721f808c7c
Daniel-Cervera <daniel.cervera.2029@gmail.com>
2024-08-31 19:54:24 -0600

    variables.tk file

---
+++ BASICS/String_Templates.kt
@@ -0,0 +1,11 @@
+// Its useful to know how to print the conntents of variable to standar output. You can do this 
+// with string templates. You can use tamplate expressions to access data stored in variables
+// and other objects, and convert them into strings. A string value is a sequence ogf characters in 
+// double quotes "". Templates expressions always start with a dollar sign $.
+
+// To evaluate a piece of code in a template expression, place the code within curly braces {}
+// after the dollar sign $.
+
+// For example:
+    val customers = 10 
+    
