3efbf4ab4808dd564485ec78f8b2264de3824196
Daniel-Cervera <daniel.cervera.2029@gmail.com>
2024-08-31 19:31:29 -0600

    add files .kt to update the guide and do it more interactive in the editor, remove .txt file hello world

---
+++ BASICS/Hello_World.kt
@@ -0,0 +1,15 @@
+//Here is a simple program that prints "Hello, World!"
+
+fun main() {
+    print("Hello, world!")
+    // Hello, world!
+}
+
+// in Kotlin 
+    - fun is use to declare a function
+    -The main()  function is written within curly braces {}
+    -println() and print() functions print their arguments to startdard output 
+
+// a function is set of instructions that performs a specific task. Once you create  a functions,
+// yu can use it whenever yopu need to perfomr that task, withoutr having to write the 
+// instructions all over again.
---
+++ BASICS/Variables.kt
@@ -0,0 +1,19 @@
+// All pograms need to able to store data, and variable help you to do just that. In Kotlin, you 
+// can declare:
+
+    - Read-only variable with val 
+    - Mutable variables with var
+
+// TO assign a value, use the assignment operator.
+//For example:
+
+    val popcorn = 5 //There are 5 boxes of popcorn 
+    val hotdog = 7 // There are 7 hotdogs 
+    val customer = 10 // There are 20 customers in the queue 
+
+    //Some customers leave the queue
+    customers = 8 
+    print(customers)
+    // 8 
+
+//As a customers is a mutable variable, its a value can be reassigned after declaration.
